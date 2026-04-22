cb0071389445ef7f4128e54b9dc66466d6977472
Naman Saxena <namansaxena2029@gmail.com>
2026-04-22 19:54:33 +0530

    add cors

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_origins=["*"],
+    allow_origins=["*"],   # only ONE line
     allow_methods=["*"],
     allow_headers=["*"],
 )
